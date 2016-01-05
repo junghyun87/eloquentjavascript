57230d35162cf82c6cec1318ea77abecae7c6b5d
Jung-Hyun Kwon <arunson@kaist.ac.kr>
2016-01-05 19:36:59 +0900

    in the middle of ch6

--- ch17/ContentNegotiation.js
+++ ch17/ContentNegotiation.js
@@ -1,3 +1,28 @@
 /**
  * Created by Jung-Hyun on 2016-01-05.
  */
+// Your code here.
+var req = new XMLHttpRequest();
+req.open("GET", "author", false);
+req.setRequestHeader("Accept","text/plain");
+req.send(null);
+console.log(req.status);
+console.log(req.getResponseHeader("content-type"));
+
+req.open("GET", "author", false);
+req.setRequestHeader("Accept","text/html");
+req.send(null);
+console.log(req.status);
+console.log(req.getResponseHeader("content-type"));
+
+req.open("GET", "author", false);
+req.setRequestHeader("Accept","application/json");
+req.send(null);
+console.log(req.status);
+console.log(req.getResponseHeader("content-type"));
+
+req.open("GET", "author", false);
+req.setRequestHeader("Accept","application/rainbows+unicorns");
+req.send(null);
+console.log(req.status);
+console.log(req.getResponseHeader("content-type"));
